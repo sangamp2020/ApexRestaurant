453b21ad10f0aaac8867124b2e93e0686bd588f0
Sangam Pudasaini <sangam.pudasaini@apexcollege.edu.np>
2020-07-19 14:07:22 +0545

    Added sql connection and tested

--- ApexRestaurant.Mvc/Startup.cs
+++ ApexRestaurant.Mvc/Startup.cs
@@ -33,7 +33,13 @@ namespace ApexRestaurant.Mvc
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
-           services.AddRazorPages();
+            services.AddRazorPages();
+
+            services.AddHttpClient();
+        //    services.AddHttpClient("API Client", client => {
+        //         client.BaseAddress = new Uri("https://www.metaweather.com/");
+        //         client.DefaultRequestHeaders.Add("Accept", "application/json");
+        //     });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
